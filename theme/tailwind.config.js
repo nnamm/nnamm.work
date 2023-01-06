833dcdd984d3e636928e5161d39351b3227dcce3
nnamm <t.nnamm@gmail.com>
2023-01-06 10:18:27 +0900

    preparing to change Hugo

--- theme/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./nnamm.work/templates/*.html', './nnamm.work/templates/**/*.html'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
